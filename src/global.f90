0293fabc1074f8d604a8ab5dddf7e481b9598e8c
Anuvab2002 <anuvabpanda2002@gmail.com>
2026-05-02 15:16:24 +0530

    global parameters

---
+++ src/global.f90
@@ -0,0 +1,10 @@
+!> @\file global.f90
+!> @\author ap
+!> brief provides values of some globally used parameters
+module global_m
+!============================ Global parameters ===============================!
+  double precision, parameter:: tolerance = 1.d-5
+  double precision, parameter:: pi= 4.d0*datan(1.d0)
+  complex(kind=8), parameter:: iota=(0.d0, 1.d0)
+!==============================================================================!
+end module global_m
